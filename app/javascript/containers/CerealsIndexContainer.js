4720972d5772d88962c34a374cae755263b4e9e7
Braden Lawrence <b@bradenlawrence.com>
2020-01-25 14:02:54 -0500

    add map to display all cereals available in state

--- app/javascript/containers/CerealsIndexContainer.js
+++ app/javascript/containers/CerealsIndexContainer.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const CerealsIndexContainer = props => {
-  return <div>I'm the Index Container!</div>
+  const [cereals, setCereals] = useState('')
+
+
+  const cerealTiles = cereals.map(cereal => {
+    return(
+      <li>
+        <span><strong>Name: </strong>{cereal.name}</span><br/>
+        <span><strong>Description: </strong>{cereal.description}</span>
+      </li>
+    )
+  })
+
+  return(
+    <div>
+      <h2>All Cereals:</h2>
+      <ul>
+        {cerealTiles}
+      </ul>
+    </div>
+  )
 }
 
 export default CerealsIndexContainer
