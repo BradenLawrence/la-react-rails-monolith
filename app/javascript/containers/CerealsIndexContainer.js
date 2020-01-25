b2cebe0ccdcf35d126c351373a76685cb5d1abab
Braden Lawrence <b@bradenlawrence.com>
2020-01-25 14:03:31 -0500

    begin defining fetch call to add cereals to props

--- app/javascript/containers/CerealsIndexContainer.js
+++ app/javascript/containers/CerealsIndexContainer.js
@@ -3,6 +3,21 @@ import React, { useState, useEffect } from 'react'
 const CerealsIndexContainer = props => {
   const [cereals, setCereals] = useState('')
 
+  useEffect(() => {
+    fetch('api/v1/cereals.json')
+      .then(response => {
+        if(response.ok) {
+          return response
+        } else {
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+      })
+      .then(validatedResponse => validatedResponse.json())
+      .then(body => setCereals(body))
+      .catch(error => {
+        console.log(`Error fetching cereal list: ${error.message}`)
+      })
+  }, [])
 
   const cerealTiles = cereals.map(cereal => {
     return(
