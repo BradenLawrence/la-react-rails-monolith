b646dfc913e313a5a8f2fc7763a6f23a6e1129fb
Braden Lawrence <b@bradenlawrence.com>
2020-01-25 13:10:57 -0500

    add seed data for five classic cereal brands

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cereal_seeds = [
+  {
+    name: "Cap'n Crunch",
+    description: "Oh god, the roof of my mouth is bleeding!"
+  },
+  {
+    name: "Cinnamon Toast Crunch",
+    description: "Wendel's secret recipe."
+  },
+  {
+    name: "Coco Puffs",
+    description: "Chocolately goodness"
+  },
+  {
+    name: "Cookie Crisp",
+    description: "Cookies? For Breakfast?!"
+  },
+  {
+    name: "Lucky Charms",
+    description: "Candy? For Breakfast?!"
+  }
+]
+
+cereal_seeds.each do |seed|
+  Cereal.create(seed)
+end
